f4ded4008d5e366abf02473058e7e4688cc6562e
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-09-01 13:49:26 +0300

    Added Multiply Function

---
+++ src/views/Home.vue
@@ -0,0 +1,185 @@
+<script setup>
+import CustomButton from "../components/CustomButton.vue";
+import { ref } from "vue";
+
+const result = ref("");
+const specialSymbols = ["C", "="];
+const calculateableSymbols = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+const operationSymbols = ["*", "/", "%", "-", "+", "="];
+
+function onButtonClick(symbol) {
+  if (symbol === "C") {
+    result.value = "";
+  } else if (
+    operationSymbols.some((symbol) => result.value.includes(symbol)) &&
+    operationSymbols.includes(symbol)
+  ) {
+    switch (symbol) {
+      case "*":
+        handleMultiply();
+        break;
+      case "=":
+        handleSolve();
+        break;
+      default:
+        break;
+    }
+  } else {
+    result.value += symbol;
+  }
+}
+
+function handleSolve() {
+  const operationSymbol = operationSymbols.filter((symbol) =>
+    result.value.includes(symbol)
+  )[0];
+
+  switch (operationSymbol) {
+    case "*":
+      handleMultiply();
+      break;
+
+    default:
+      break;
+  }
+}
+
+function handleMultiply() {
+  const numbers = result.value.split("*");
+
+  result.value = `${numbers[0] * numbers[1]}`;
+}
+</script>
+
+<template>
+  <main>
+    <div id="calculator">
+      <div class="input-box-wrapper">
+        <input type="text" name="input-box" id="input-box" :value="result" readonly />
+      </div>
+      <div class="buttons-row">
+        <CustomButton backgroundColor="green" symbol="C" :onButtonClick="onButtonClick" />
+        <CustomButton backgroundColor="green" symbol="*" :onButtonClick="onButtonClick" />
+        <CustomButton backgroundColor="green" symbol="/" :onButtonClick="onButtonClick" />
+        <CustomButton backgroundColor="green" symbol="-" :onButtonClick="onButtonClick" />
+      </div>
+      <div class="buttons-row">
+        <CustomButton
+          backgroundColor="secondary"
+          symbol="7"
+          :onButtonClick="onButtonClick"
+        />
+        <CustomButton
+          backgroundColor="secondary"
+          symbol="8"
+          :onButtonClick="onButtonClick"
+        />
+        <CustomButton
+          backgroundColor="secondary"
+          symbol="9"
+          :onButtonClick="onButtonClick"
+        />
+        <CustomButton backgroundColor="green" symbol="+" :onButtonClick="onButtonClick" />
+      </div>
+      <div class="buttons-row">
+        <CustomButton
+          backgroundColor="secondary"
+          symbol="4"
+          :onButtonClick="onButtonClick"
+        />
+        <CustomButton
+          backgroundColor="secondary"
+          symbol="5"
+          :onButtonClick="onButtonClick"
+        />
+        <CustomButton
+          backgroundColor="secondary"
+          symbol="6"
+          :onButtonClick="onButtonClick"
+        />
+        <CustomButton backgroundColor="green" symbol="%" :onButtonClick="onButtonClick" />
+      </div>
+      <div class="buttons-row">
+        <CustomButton
+          backgroundColor="secondary"
+          symbol="1"
+          :onButtonClick="onButtonClick"
+        />
+        <CustomButton
+          backgroundColor="secondary"
+          symbol="2"
+          :onButtonClick="onButtonClick"
+        />
+        <CustomButton
+          backgroundColor="secondary"
+          symbol="3"
+          :onButtonClick="onButtonClick"
+        />
+        <CustomButton backgroundColor="green" symbol="=" :onButtonClick="onButtonClick" />
+      </div>
+      <div class="buttons-row">
+        <CustomButton
+          backgroundColor="secondary"
+          symbol="0"
+          :onButtonClick="onButtonClick"
+        />
+        <CustomButton
+          backgroundColor="secondary"
+          symbol="."
+          :onButtonClick="onButtonClick"
+        />
+      </div>
+    </div>
+  </main>
+</template>
+
+<style>
+main {
+  min-height: 100vh;
+  margin: 4rem 0;
+  display: flex;
+  justify-content: center;
+}
+
+#calculator {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 20px;
+  border-radius: 5px;
+  width: 400px;
+  height: 550px;
+  background-color: #252f45;
+}
+
+.buttons-row {
+  display: flex;
+  justify-content: space-between;
+  gap: 20px;
+}
+
+.buttons-row:last-child {
+  justify-content: flex-start;
+}
+
+.input-box-wrapper {
+  width: 100%;
+}
+
+#input-box {
+  width: 100%;
+  padding: 12px;
+  height: 80px;
+  background-color: var(--secondary) !important;
+  border: 1px solid var(--primary);
+  border-radius: 4px;
+  font-size: 1.5rem;
+  color: white;
+  cursor: pointer;
+}
+
+#input-box:focus-visible {
+  outline: none;
+  border: 2px solid var(--primary) !important;
+}
+</style>
