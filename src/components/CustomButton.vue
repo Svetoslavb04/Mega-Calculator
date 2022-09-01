c52aed556ff31e41374bf6b5ee5c3e5b666f18c2
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-09-01 12:39:45 +0300

    Created Calculator Design

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,35 @@
+<script setup>
+const props = defineProps(["backgroundColor", "symbol"]);
+
+const backgroundColor = props.backgroundColor;
+const symbol = props.symbol;
+</script>
+<template>
+  <div class="custom-button" :class="backgroundColor">
+    <h1 class="custom-button-symbol">
+      {{ symbol }}
+    </h1>
+  </div>
+</template>
+<style>
+.custom-button {
+  width: 75px;
+  height: 75px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.custom-button-symbol {
+    color: white;
+}
+
+.green {
+  background-color: #1cc883;
+}
+
+.grey {
+  background-color: #35425f;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,91 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import CustomButton from "../components/CustomButton.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div id="calculator">
+      <div class="input-box-wrapper">
+        <input type="text" name="input-box" id="input-box" />
+      </div>
+      <div class="buttons-row">
+        <CustomButton backgroundColor="green" symbol="C" />
+        <CustomButton backgroundColor="green" symbol="*" />
+        <CustomButton backgroundColor="green" symbol="/" />
+        <CustomButton backgroundColor="green" symbol="-" />
+      </div>
+      <div class="buttons-row">
+        <CustomButton backgroundColor="grey" symbol="7" />
+        <CustomButton backgroundColor="grey" symbol="8" />
+        <CustomButton backgroundColor="grey" symbol="9" />
+        <CustomButton backgroundColor="green" symbol="+" />
+      </div>
+      <div class="buttons-row">
+        <CustomButton backgroundColor="grey" symbol="4" />
+        <CustomButton backgroundColor="grey" symbol="5" />
+        <CustomButton backgroundColor="grey" symbol="6" />
+        <CustomButton backgroundColor="green" symbol="%" />
+      </div>
+      <div class="buttons-row">
+        <CustomButton backgroundColor="grey" symbol="1" />
+        <CustomButton backgroundColor="grey" symbol="2" />
+        <CustomButton backgroundColor="grey" symbol="3" />
+        <CustomButton backgroundColor="green" symbol="=" />
+      </div>
+      <div class="buttons-row">
+        <CustomButton backgroundColor="grey" symbol="0" />
+        <CustomButton backgroundColor="grey" symbol="." />
+      </div>
+    </div>
   </main>
 </template>
+
+<style>
+main {
+  min-height: 100vh;
+  margin: 4rem 0;
+  display: flex;
+  justify-content: center;
+}
+
+#calculator {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 20px;
+  border-radius: 5px;
+  width: 400px;
+  height: 550px;
+  background-color: #252f45;
+}
+
+.buttons-row {
+  display: flex;
+  justify-content: space-between;
+  gap: 20px;
+}
+
+.buttons-row:last-child {
+  justify-content: flex-start;
+}
+
+.input-box-wrapper {
+  width: 100%;
+}
+
+#input-box {
+  width: 100%;
+  padding: 12px;
+  height: 80px;
+  background-color: #35425f !important;
+  border: 1px solid #1cc883;
+  border-radius: 4px;
+  font-size: 1.5rem;
+  color: white;
+}
+
+#input-box:focus-visible {
+  outline: none;
+  border: 2px solid #1cc883 !important;
+}
+</style>
